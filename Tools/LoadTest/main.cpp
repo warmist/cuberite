3d77e095331625c2a71f9a78333731adb13b4a86
tycho <work.tycho@gmail.com>
2015-09-25 15:51:40 +0100

    Add teleporting support

--- Tools/LoadTest/main.cpp
+++ Tools/LoadTest/main.cpp
@@ -57,6 +57,7 @@ void LOG(const char * a_Format, ...)
 #include "Globals.h"
 #include "../../src/OSSupport/Network.h"
 #include "../../src/ByteBuffer.h"
+#include "../../src/FastRandom.h"
 
 
 class ConnectionCallbacks : public cNetwork::cConnectCallbacks
@@ -83,12 +84,16 @@ class LinkCallbacks : public cTCPLink::cCallbacks
 	} m_State;
 
 	int id;
+	bool m_teleport, m_portal;
+	cTCPLinkPtr m_Link;
 
 public:
-	LinkCallbacks(int i) : m_State(LOGIN), id(i), recivedData(1024) {}
+	LinkCallbacks(int i, bool a_teleport, bool a_portal) : m_State(LOGIN), id(i), m_teleport(a_teleport), m_portal(a_portal), recivedData(10240) {}
+	~LinkCallbacks() { std::cout << "Link Deleted" << std::endl; }
 
 	virtual void OnLinkCreated(cTCPLinkPtr a_Link)
 	{
+		m_Link = a_Link;
 		cByteBuffer p(400);
 		// Handshake
 		p.WriteVarInt32(15);
@@ -112,10 +117,13 @@ public:
 		p.ReadAll(data);
 		p.CommitRead();
 		a_Link->Send(data);
+
 	}
+
+#define CHECK_CALL(x) if (!(x)) {m_Link->Close(); m_Link = nullptr; return; }
 	virtual void OnReceivedData(const char * a_Data, size_t a_Length)
 	{
-		recivedData.WriteBuf(a_Data, a_Length);
+		CHECK_CALL(recivedData.WriteBuf(a_Data, a_Length));
 		switch (m_State)
 		{
 			case LOGIN:
@@ -127,8 +135,46 @@ public:
 		}
 	}
 
-#define IGNORE_PAYLOAD recivedData.SkipRead(length - typeLength);
 
+
+
+	void Teleport()
+	{
+		std::stringstream messageStream;
+		cFastRandom rd;
+		messageStream << "/tp " << rd.GenerateRandomInteger(-100000, 100000) << " " << rd.GenerateRandomInteger(0, 255) << " " << rd.GenerateRandomInteger(-100000, 100000);
+		std::cout << "teleporting" << std::endl;
+		Message(messageStream.str());
+	}
+
+
+
+
+	void Portal()
+	{
+		Message("/portal second");
+	}
+
+
+	
+	void Message(std::string a_message)
+	{
+		
+		cByteBuffer packet(4000);
+		// Handshake
+		cByteBuffer payload(1000);
+		payload.WriteVarInt32(0x01);
+		payload.WriteVarUTF8String(a_message);
+		packet.WriteVarInt32(payload.GetUsedSpace());
+		payload.ReadToByteBuffer(packet, payload.GetUsedSpace());
+		std::string data;
+		packet.ReadAll(data);
+		packet.CommitRead();
+		m_Link->Send(data);
+	}
+
+
+#define IGNORE_PAYLOAD recivedData.SkipRead(length - typeLength);
 	void HandleLoginPackets()
 	{
 		for(;;) 
@@ -145,7 +191,7 @@ public:
 				break;
 			}
 			int typeLength = recivedData.GetReadableSpace();
-			assert(recivedData.ReadVarInt32(type));
+			CHECK_CALL(recivedData.ReadVarInt32(type));
 			typeLength -= recivedData.GetReadableSpace();
 			switch (type)
 			{
@@ -155,12 +201,14 @@ public:
 					IGNORE_PAYLOAD
 					recivedData.CommitRead();
 					m_State = GAME;
+					if (m_portal) Portal();
+					if (m_teleport) Teleport();
 					HandleGamePackets();
 					break;
 				}
 				default:
 				{
-					std::cout << id << ": Unknown: Ignoring: " << type << std::endl;
+					std::cout << id << ": Unknown Login: Ignoring: " << std::hex << type << std::endl;
 					IGNORE_PAYLOAD
 					break;
 				}
@@ -168,7 +216,6 @@ public:
 			recivedData.CommitRead();
 		}
 	}
-
 
 	void HandleGamePackets()
 	{
@@ -191,10 +238,27 @@ public:
 				break;
 			}
 			int typeLength = recivedData.GetReadableSpace();
-			assert(recivedData.ReadVarInt32(type));
+			CHECK_CALL(recivedData.ReadVarInt32(type));
 			typeLength -= recivedData.GetReadableSpace();
 			switch (type)
 			{
+				case 0x0:
+				{
+					int32_t id;
+					CHECK_CALL(recivedData.ReadBEInt32(id));
+					cByteBuffer packet(100);
+					cByteBuffer payload(100);
+					payload.WriteVarInt32(0x0);
+					payload.WriteBEInt32(id);
+					packet.WriteVarInt32(payload.GetUsedSpace());
+					payload.ReadToByteBuffer(packet, payload.GetUsedSpace());
+					std::string data;
+					packet.ReadAll(data);
+					packet.CommitRead();
+					recivedData.CommitRead();
+					m_Link->Send(data);
+					break;	
+				}
 				case 0x2:
 				{
 					std::cout << id << ": Chat" << std::endl;
@@ -203,7 +267,7 @@ public:
 				}
 				default:
 				{
-					std::cout << id << ": Unknown: Ignoring: " << type << std::endl;
+					std::cout << id << ": Unknown game: Ignoring: " << std::hex << type << std::endl;
 					IGNORE_PAYLOAD
 					break;
 				}
@@ -214,10 +278,12 @@ public:
 
 	virtual void OnRemoteClosed(void)
 	{
+		m_Link = nullptr;
 		std::cout << id << ": closed" << std::endl;
 	}
 	virtual void OnError(int a_ErrorCode, const AString & a_ErrorMsg)
 	{
+		m_Link = nullptr;
 		std::cout << id << ": error" << std::endl;
 	}
 
@@ -227,12 +293,22 @@ private:
 };
 
 
-int main()
+int main(int argc, char** argv)
 {
-	for(int i = 0;i < 50000; i++)
+	bool teleport = false;
+	bool portal = false;
+	if (argc > 1)
+	{
+		if (std::string{argv[1]} == "-t") teleport = true;
+		if (std::string{argv[1]} == "-p") portal = true;
+	}
+	std::vector<std::shared_ptr<LinkCallbacks>> links;
+	for(int i = 0;i < 200; i++)
 	{
 		std::cout << "Creating new Player" << std::endl;
-		cNetwork::Connect("localhost", 25565, std::make_shared<ConnectionCallbacks>(), std::make_shared<LinkCallbacks>(i));
+		auto callback = std::make_shared<LinkCallbacks>(i, teleport, portal);
+		links.push_back(callback);
+		cNetwork::Connect("localhost", 25565, std::make_shared<ConnectionCallbacks>(), callback);
 		usleep(100000);
 	}
 	for(;;);
