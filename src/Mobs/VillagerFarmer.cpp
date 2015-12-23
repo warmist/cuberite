08f6b2ca6810f1485597cf5f7754b8a72117b766
Julian Laubstein <julianlaubstein@yahoo.de>
2015-12-23 12:38:50 +0100

    Added Farmer class

---
+++ src/Mobs/VillagerFarmer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by sphinxc0re on 12/23/15.
+//
+
+#include "cVillagerFarmer.h"
---
+++ src/Mobs/VillagerFarmer.h
@@ -0,0 +1,14 @@
+//
+// Created by sphinxc0re on 12/23/15.
+//
+
+#ifndef CUBERITE_CVILLAGERFARMER_H
+#define CUBERITE_CVILLAGERFARMER_H
+
+
+class cVillagerFarmer {
+
+};
+
+
+#endif //CUBERITE_CVILLAGERFARMER_H
